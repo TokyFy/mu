235e97dc08b4c78ab27b5871473ceae04804f927
TokyFy <tokyfydev@gmail.com>
2023-07-29 12:13:59 +0200

    :ambulance: Fix Language when changing route

--- components/ui/DropDown.tsx
+++ components/ui/DropDown.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import Link from "next/link";
+import Link from 'next-intl/link';
+import {usePathname} from 'next-intl/client';
+
 
 type props = {
     trigger:string
 }
 
 const Dropdown = ({trigger} : props) => {
+
+    const pathname = usePathname();
+
+    useEffect(() => {
+        console.log(pathname)
+    }, [pathname]);
+
 
     return (
         <DropdownMenu.Root>
@@ -19,19 +28,19 @@ const Dropdown = ({trigger} : props) => {
             <DropdownMenu.Portal>
                 <DropdownMenu.Content className="text-center bg-neutral-200 text-primary-dark p-2 px-4 font-bold bottom-2 border-primary-dark z-50" sideOffset={5}>
                     <DropdownMenu.Item  className="cursor-pointer my-2 hover:underline">
-                       <Link href={"/fr"}>Français</Link>
+                       <Link href={pathname} locale={"fr"}>Français</Link>
                     </DropdownMenu.Item>
 
                     <DropdownMenu.Item  className="cursor-pointer my-2 hover:underline">
-                        <Link href={"/en"}>English</Link>
+                        <Link href={pathname}  locale={"en"}>English</Link>
                     </DropdownMenu.Item>
 
                     <DropdownMenu.Item  className="cursor-pointer my-2 hover:underline">
-                        <Link href={"/de"}>Deutsch</Link>
+                        <Link href={pathname} locale={"de"}>Deutsch</Link>
                     </DropdownMenu.Item>
 
                     <DropdownMenu.Item  className="cursor-pointer my-2 hover:underline">
-                        <Link href={"/ko"}>한국인</Link>
+                        <Link href={pathname} locale={"ko"}>한국인</Link>
                     </DropdownMenu.Item>
 
                     <DropdownMenu.Arrow/>
